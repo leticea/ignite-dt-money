5b7b91636895b8f06491ef6b819a6127d49d3962
leticea <nascimento.leticeah@gmail.com>
2023-07-05 17:59:47 -0300

    feat(Summary): creating summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryContainer } from "./styles";
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <div>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <span>Entradas</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </div>
+    </SummaryContainer>
+  );
+}
