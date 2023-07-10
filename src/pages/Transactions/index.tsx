8578635af8aaed5cae8d7dcaf4817d3d5dfe1485
leticea <nascimento.leticeah@gmail.com>
2023-07-10 17:37:52 -0300

    feat(Transactions): price styles

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,10 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
-import { TransactionsContainer, TransactionsTable } from "./styles";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
@@ -13,13 +17,17 @@ export function Transactions() {
           <tbody>
             <tr>
               <td width="50%">Desenvolvimento de site</td>
-              <td>R$ 12.000,00</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
               <td>Venda</td>
               <td>13/04/2023</td>
             </tr>
             <tr>
               <td width="50%">Hambúrguer</td>
-              <td>- R$ 59,00</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
               <td>Alimentação</td>
               <td>10/04/2023</td>
             </tr>
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -28,3 +28,14 @@ export const TransactionsTable = styled.table`
     }
   }
 `;
+
+interface PriceHighlightProps {
+  variant: "income" | "outcome";
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${(props) =>
+    props.variant === "income"
+      ? props.theme["green-300"]
+      : props.theme["red-300"]};
+`;
