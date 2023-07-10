1f26a4248d3d995dc9260abb63280d0966c5f869
leticea <nascimento.leticeah@gmail.com>
2023-07-10 17:54:37 -0300

    feat(SearchForm): styles

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,5 +1,14 @@
+import { MagnifyingGlass } from "phosphor-react";
+import { SearchFormContainer } from "./styles";
+
 export function SearchForm() {
   return (
-    <div></div>
+    <SearchFormContainer>
+      <input type="text" placeholder="Busque por transações" />
+
+      <button type="submit">
+        <MagnifyingGlass size={20} />
+        Buscar</button>
+    </SearchFormContainer>
   )
 }
