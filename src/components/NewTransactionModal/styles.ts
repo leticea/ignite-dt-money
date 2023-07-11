77ddf3ba8082ade203dbcc7d7394012210873460
leticea <nascimento.leticeah@gmail.com>
2023-07-11 10:39:00 -0300

    feat(NewTransactionModal): styles

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -19,4 +19,40 @@ export const Content = styled(Dialog.Content)`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
+  form {
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    input {
+      border-radius: 6px;
+      border: 0;
+      background: ${(props) => props.theme["gray-900"]};
+      color: ${(props) => props.theme["gray-300"]};
+      padding: 1rem;
+
+      &::placeholder {
+        color: ${(props) => props.theme["gray-500"]};
+      }
+    }
+
+    button[type="submit"] {
+      height: 58px;
+      border: 0;
+      background: ${(props) => props.theme["green-500"]};
+      color: ${(props) => props.theme.white};
+      font-weight: bold;
+      padding: 0 1.25rem;
+      border-radius: 6px;
+      margin-top: 1.5rem;
+      cursor: pointer;
+
+      &:hover {
+        background: ${(props) => props.theme["green-700"]};
+        transition: background-color 0.2s;
+      }
+    }
+  }
 `;
