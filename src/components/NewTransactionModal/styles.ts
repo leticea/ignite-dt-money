c32db9c82b0ef96ff9d6f53ff1eb2c983dd12bec
leticea <nascimento.leticeah@gmail.com>
2023-07-11 10:31:09 -0300

    feat(NewTransactionModal): styles

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,6 +1,22 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import styled from "styled-components";
 
-export const Overlay = styled(Dialog.Overlay)``;
+export const Overlay = styled(Dialog.Overlay)`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.75);
+`;
 
-export const Content = styled(Dialog.Content)``;
+export const Content = styled(Dialog.Content)`
+  min-width: 32rem;
+  border-radius: 6px;
+  padding: 2.5rem 3rem;
+  background: ${(props) => props.theme["gray-800"]};
+
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
