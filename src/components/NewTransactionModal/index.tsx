7a9f505122621044aac062da4fd745be8cc4e768
leticea <nascimento.leticeah@gmail.com>
2023-07-11 10:24:38 -0300

    feat(NewTransactionModal): creating modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,26 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { Content, Overlay } from "./styles";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+
+        <form action="">
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <button type="submit">
+            Cadastrar
+          </button>
+        </form>
+
+        <Dialog.Close />
+      </Content>
+    </Dialog.Portal>
+  );
+}
---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,6 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import styled from "styled-components";
+
+export const Overlay = styled(Dialog.Overlay)``;
+
+export const Content = styled(Dialog.Content)``;
