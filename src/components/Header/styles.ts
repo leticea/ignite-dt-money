48057e0afc329ee299aa9b0aa75c404b2340507d
leticea <nascimento.leticeah@gmail.com>
2023-07-11 14:31:09 -0300

    feat(NewTransactionModal):button styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -18,6 +18,7 @@ export const HeaderContent = styled.div`
 export const NewTransactionButton = styled.button`
   height: 50px;
   border: 0;
+
   background: ${(props) => props.theme["green-500"]};
   color: ${(props) => props.theme.white};
   font-weight: bold;
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Content, Overlay } from "./styles";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
   return (
@@ -9,17 +16,28 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>Nova transação</Dialog.Title>
 
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
         <form action="">
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
 
-          <button type="submit">
-            Cadastrar
-          </button>
-        </form>
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
-        <Dialog.Close />
+          <button type="submit">Cadastrar</button>
+        </form>
       </Content>
     </Dialog.Portal>
   );
