f912e65d01042328d0250108fe05dc1efb6ca109
leticea <nascimento.leticeah@gmail.com>
2023-07-11 10:06:57 -0300

    feat(Header): inserting radix-ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <NewTransactionButton>Nova transação</NewTransactionButton>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
