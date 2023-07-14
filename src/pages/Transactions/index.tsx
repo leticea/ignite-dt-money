c203fb96c1f1e77da47a9f1288b9606bc9b7c947
leticea <nascimento.leticeah@gmail.com>
2023-07-14 15:55:06 -0300

    feat(Transactions): price format

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -8,7 +8,7 @@ import {
   TransactionsTable,
 } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { priceFormatter } from "../../utils/formatter";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext);
@@ -33,7 +33,9 @@ export function Transactions() {
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               );
             })}
