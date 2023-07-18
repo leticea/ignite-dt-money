6fcd59ac56137e7ce5daecbaeea0c2e821ca24e9
leticea <nascimento.leticeah@gmail.com>
2023-07-18 17:47:03 -0300

    feat(readme): creating readme file

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -42,5 +42,11 @@ export const SearchFormContainer = styled.form`
       color: ${(props) => props.theme.white};
       transition: background-color 0.2s, color 0.2s, border-color 0.2s;
     }
+
+    @media screen and (max-width: 768px) {
+      margin-left: -0.6rem;
+      width: 105px;
+      gap: 0.25rem;
+    }
   }
 `;
